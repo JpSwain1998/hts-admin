e695d48b2b8020cdabbc216ccc8434089b13dfe6
JpSwain1998 <124132658+JpSwain1998@users.noreply.github.com>
2026-04-17 03:43:14 +0530

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Hindustan Tech Solution</h1>
+      <h2>Admin Dashboard</h2>
+
+      <div style={{ marginTop: 20 }}>
+        <p>Total Tickets: 120</p>
+        <p>Open: 30</p>
+        <p>Closed: 90</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
